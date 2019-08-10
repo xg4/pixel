1c7b90108bb3620e25f328f68bb83e7e3bd1d3f4
xg4 <xingor4@gmail.com>
2019-08-10 15:54:17 +0800

    chore: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,7 +26,8 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'standard', 'jest', 'prettier', 'react'],
   rules: {
-    'no-console': process.env.NODE_ENV !== 'production' ? 0 : 2,
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/no-explicit-any': 0
   }
